12a78010d077b35cd0fd86e555522fe726dcf7ac
Kyle Wood <97246548+huxley15@users.noreply.github.com>
2022-02-08 14:32:11 -0800

    Add files via upload

---
+++ Week 4/Assignment4.2 part 2/MathCls.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment4._2_part_2
+{
+    internal class MathCls
+    {
+        
+        public int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+        public int Add(int num1, int num2, int num3)
+        {
+            return num1+num2 + num3;
+        }
+
+        public float Multiply(float num1, float num2)
+        {
+            return num1 * num2;
+        }
+        public float Multiply(float num1, float num2, float num3)
+        {
+            return num1 * num2 * num3;
+        }
+    }
+}
