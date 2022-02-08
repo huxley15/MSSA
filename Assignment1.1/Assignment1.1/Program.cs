67dd4f1acf49b9c22e846bc847f406a9d7b8b3d4
Kyle Wood <97246548+huxley15@users.noreply.github.com>
2022-02-08 11:41:42 -0800

    Add files via upload
    Create a simple console application and store your name, age (should take decimal), address in a string variable and print them back on console.

---
+++ Assignment1.1/Assignment1.1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.BackgroundColor = ConsoleColor.White;
+            float age;
+            string firstName;
+            string address;
+
+            // Console.WriteLine("float max value : {0}", float.MaxValue);
+
+            Console.WriteLine("Please enter your name:");
+            firstName = Console.ReadLine();
+            Console.WriteLine("Please enter your age:");
+            age =Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine("Please enter your street address:");
+            address = Console.ReadLine();
+            // Console.ReadKey();
+            // Console.ReadLine();
+
+            Console.WriteLine("Your information:");
+            Console.WriteLine("Name is {0} , age is {1} , address is {2}", firstName, age, address);
+            Console.ReadLine();
+        }
+    }
+}
