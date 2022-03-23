f7eb6a32dd8e745742c8744ac6e1dcb4ae99bcf5
Kyle Wood <97246548+huxley15@users.noreply.github.com>
2022-03-22 22:33:03 -0700

    Add files via upload

---
+++ Week 10/Assignment 10.2/DOMscript.js
@@ -0,0 +1,28 @@
+﻿document.addEventListener('DOMContentLoaded', function (event) {
+    document.getElementById('btnAddHobby').addEventListener('click', addHobby);
+});
+
+function addHobby() {
+    //get all hobbies input text boxes
+    const inputList = document.querySelectorAll('.hobbyInput');
+    //check if hobbies are less than 5
+    if (inputList.length < 5) {
+        const hobbyList = document.getElementById('hobbyList');
+        const newlineelement = CreateNode('br'), //create br
+            inputElement = CreateNode('input'); //create input
+        inputElement.setAttribute("class", "hobbyInput");
+        append(hobbyList, newlineelement); //add br into div
+        append(hobbyList, inputElement); //add input text box into div
+    }
+    else {
+        document.getElementById('btnAddHobby').removeEventListener('click', addHobby);
+    }
+}
+
+function CreateNode(element) {
+    return document.createElement(element);
+}
+
+function append(parent, el) {
+    return parent.appendChild(el);
+}
