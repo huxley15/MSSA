3068b02a4446735846fa55cdf2d3cf49d6a211ac
Kyle Wood <97246548+huxley15@users.noreply.github.com>
2022-03-30 11:48:49 -0700

    Add files via upload

---
+++ Week 11/Assignment 11.4/Services/IData.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Assignment11._4.Model;
+
+namespace Assignment11._4.Services
+    
+{
+    public interface IData
+    {
+        IEnumerable<Product> GetAll();
+        Product GetProduct(int id);
+        void AddProduct(Product product);
+        void UpdateProduct(Product product);
+        void DeleteProduct(int id);
+    }
+}
---
+++ Week 11/Assignment 11.4/Services/ProductData.cs
@@ -0,0 +1,33 @@
+﻿using Assignment11._4.Model;
+using System.Collections.Generic;
+
+namespace Assignment11._4.Services
+{
+    public class ProductData : IData
+    {
+        public void AddProduct(Product product)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void DeleteProduct(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<Product> GetAll()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Product GetProduct(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void UpdateProduct(Product product)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
