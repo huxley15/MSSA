908c20fdb16fe88f9ca81033c5b465e61e177b34
Kyle Wood <97246548+huxley15@users.noreply.github.com>
2022-04-02 15:18:48 -0700

    Add files via upload

---
+++ Demos/10975/Mod10Demo2/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod10Demo2
+{
+    class Class1:IDisposable
+    {
+        ~Class1() //nondeterministic
+        {
+            //cleaning 
+        }
+
+        public void Dispose() //deterministic
+        {
+            GC.SuppressFinalize(this);
+            //cleaning unmanaged resources
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Demos/10975/Mod10Demo2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Mod10Demo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 obj = new Class1();
+            obj.Dispose(); //deterministic
+
+
+            using (Class1 obj1 = new Class1())
+            {
+                //calls dispose automatically
+            }
+
+            using (SqlConnection con = new SqlConnection())
+            {
+
+            }
+        }
+    }
+}
