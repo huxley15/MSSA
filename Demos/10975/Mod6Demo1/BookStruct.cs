34c886e40e46d0bc0cfd373f1e5f1e283474f913
Kyle Wood <97246548+huxley15@users.noreply.github.com>
2022-04-02 14:57:39 -0700

    Add files via upload

---
+++ Demos/10975/Mod6Demo1/BookStruct.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod6Demo1
+{
+    
+    //a structure is a value type user defined complex data structure that may represent real life object
+    //and its a collection of basic data types like int, string, float
+    struct Book
+    {
+        //member fields
+        /*public int bookId;
+        public string bookName;
+        public string bookAuthor; */
+
+        private int bookId;
+        private string bookName;
+        private string bookAuthor;
+        //a constructor is a special function having the same name of the struct/class
+        //which intializes the data memebers and allocates memory
+        
+        //1st way
+        public Book(int id,string name, string author)
+        {
+            //initialization of the data members
+            this.bookId = id;
+            this.bookName = name;
+            this.bookAuthor = author;
+        }
+
+        public string _BookName
+        {
+            get
+            {
+                return this.bookName;
+            }
+            //for a read only property, remove the set block
+            set
+            {
+                //validations
+                this.bookName = value;
+            }
+        }
+
+        public string _BookAuthor
+        {
+            get
+            {
+                return this.bookAuthor;
+            }
+            set
+            {
+                this.bookName = value;
+            }
+        }
+        public int _BookId
+        {
+            get
+            {
+                return this.bookId;
+            }
+            set
+            {
+                if (value >= 100)
+                {
+                    this.bookId = value;
+                }
+            }
+        }
+        public void DisplayBook()
+        {
+            Console.WriteLine($"Book details: {this.bookId}, {this.bookName}, {this.bookAuthor}"); 
+        }
+    }
+}
---
+++ Demos/10975/Mod6Demo1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod6Demo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book1 = new Book(); //memory allocation, members get initialized to default values
+            book1._BookId = 100;
+            book1._BookName = "asp programming";
+            Console.WriteLine($"Book details: {book1._BookId} , {book1._BookName}");
+
+            book1.DisplayBook();
+            Book book2 = new Book(101, "asp.net mvc", "xyz");
+            book2.DisplayBook();
+            
+
+            /*book1.bookId = 101; //unique
+            book1.bookName = "Learning C#";
+            book1.bookAuthor = "Wrox";
+            Console.WriteLine($"Book details: {book1.bookId} , {book1.bookName} , {book1.bookAuthor}");*/
+            //Console.WriteLine(book1.ToString());
+            Console.ReadLine();
+        }
+    }
+}
+
