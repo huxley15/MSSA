dc2acf75b923bceb775b0c590bb05c8b06fc20e0
Kyle Wood <97246548+huxley15@users.noreply.github.com>
2022-04-02 14:48:08 -0700

    Add files via upload

---
+++ Demos/10975/FirstHelloWorld/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;//highest level namespace
+using System.Collections.Generic;
+using System.Linq;//linq: language integrated queries
+using System.Text;
+using System.Threading.Tasks;
+// namespace: logical grouping of classes or sub namespaces which may / may not be related to each other
+//must include sub namespace to invoke the methods of the classes named
+
+namespace FirstHelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)  //entry point
+        {
+            
+            Console.WriteLine("Hello World");
+            Console.ReadLine();
+        }
+    }
+}
