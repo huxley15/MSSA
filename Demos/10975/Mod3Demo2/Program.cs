8ebf9791bbfac3f4a924104430212e5ad7ca7c72
Kyle Wood <97246548+huxley15@users.noreply.github.com>
2022-04-02 14:51:15 -0700

    Add files via upload

---
+++ Demos/10975/Mod3Demo2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod3Demo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter any number:");
+            int num1 = Int32.Parse(Console.ReadLine());
+            int num2 = Int32.Parse(Console.ReadLine());
+            if (num1 > 100)
+            {
+                if (num1 > 300)
+                {
+                    Console.WriteLine("Greater than 300");
+                }
+                else if (num1 > 200)
+                {
+                    Console.WriteLine("Greater than 200");
+                }
+                else if (num1 > 150)
+                {
+                    Console.WriteLine("Greater than 150");
+                }
+                else
+                {
+                    Console.WriteLine("Number is between 100 and 150");
+                }
+            }
+            if((num1 == 100) && (num2==100)) //logical AND
+                {
+
+                }
+            if ((num1 == 100) || (num2 == 100)) //logical OR
+                {
+
+                }
+            if (num1 >= 100)
+                {
+
+                }
+            if(num1 <= 100)
+                {
+
+                }
+            if(num1!=40) //not equal to
+            else
+                {
+
+                }
+            }
+        }
+    }
+}
