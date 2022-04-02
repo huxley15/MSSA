2e45e95828ed2a28ba006c79bca602e99ffbeb97
Kyle Wood <97246548+huxley15@users.noreply.github.com>
2022-04-02 14:55:39 -0700

    Add files via upload

---
+++ Demos/10975/Mod4Demo3/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod4Demo3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[] { 100, 23, 150, 2, 12 };
+            BubbleSort(numbers);
+            Console.WriteLine("Sorted list:");
+            foreach(int i in numbers)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadKey();
+        }
+
+        static void BubbleSort(int []data)
+        {
+            try
+            {
+                int temp = 0;
+                for (int i = 1; i <= data.Length; i++)
+                {
+                    for (int j = 0; j < (data.Length - 1); j++)
+                    {
+                        if (data[j + 1] < data[j]) //if next index location is less than current index location
+                        {
+                            temp = data[j];
+                            data[j] = data[j + 1];
+                            data[j + 1] = temp;
+                            //swap
+                        }
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+            }
+        }
+    }
+}
