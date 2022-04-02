8c70e0275a62ae270b47d27d641a5b3cf1b9855b
Kyle Wood <97246548+huxley15@users.noreply.github.com>
2022-04-02 14:49:18 -0700

    Add files via upload

---
+++ Demos/10975/Mod3Demo1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod3Demo1
+{
+    class Program
+    {
+        static void PrintName(string name)
+        {
+            Console.WriteLine($"Hello {name}"); //$ causes everything written to be considered for part of string
+
+        }
+        static void Main(string[] args) // entry point 
+        {
+            //#region declarations
+            //int i; // integer is faster, temp allocation for full loop
+            //i = 20;
+            //short j; // better for storing data within structure of class
+            //j = 20;
+            //long k = 5645454555;
+            //bool answer = true;
+            //const float rateofInterest = 3.4F; // const keyword is constant, cannot reassign value
+            //#endregion
+
+            /* #region variabledeclarations
+            string name = "xyz";
+            char c = 'N';
+            #endregion */
+
+            Console.WriteLine("Enter 2 numbers:");
+            Console.WriteLine("Enter num1:");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter num2:");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+
+            int result= addNumbers(number1, number2);
+
+            Console.WriteLine($"The result is {result}");
+
+            PrintName("World");
+            Console.ReadLine();
+
+        }
+
+
+        //function definition access specifier, return type, name(parameters - specify data type and name)
+        static int addNumbers(int num1,int num2)
+        {
+            //logic
+            return num1 + num2;
+
+        }
+    }
+}
