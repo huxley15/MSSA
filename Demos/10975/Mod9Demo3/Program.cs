c89055fdce008fdaf528490b382d12b245fef342
Kyle Wood <97246548+huxley15@users.noreply.github.com>
2022-04-02 15:16:14 -0700

    Add files via upload

---
+++ Demos/10975/Mod9Demo3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Mod9Demo3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DriveInfo driveInfo = new DriveInfo(@"C:\");
+            Console.WriteLine("Name:" + driveInfo.Name + "Free space:"+driveInfo.AvailableFreeSpace);
+            DirectoryInfo[] cDirs = new DirectoryInfo(@"C:\").GetDirectories();
+            using (StreamWriter sw = new StreamWriter("Cdriveinfo.txt"))
+            {
+                foreach (DirectoryInfo info in cDirs)
+                {
+                    sw.WriteLine(info.Name);
+                    
+                }
+            }
+
+            DirectoryInfo dirs = new DirectoryInfo(@"C:\MSSAfiles\");
+            FileInfo[] fileInfos = dirs.GetFiles();
+            using (StreamWriter sw = new StreamWriter("Fileinfo.txt"))
+            {
+                foreach (FileInfo info in fileInfos)
+                {
+                    sw.WriteLine(info.FullName + " " + info.Length);
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
