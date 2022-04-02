0229ead63ebdd2347081c7893f69399ec1ba31e4
Kyle Wood <97246548+huxley15@users.noreply.github.com>
2022-04-02 14:58:32 -0700

    Add files via upload

---
+++ Demos/10975/Mod6Demo2/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod6Demo2
+{
+    struct Address
+    {
+        public int AptNo { get; set; }
+        public string StreetName { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+
+
+        public Address(int aptno,string streetname,string city,string state)
+        {
+            this.AptNo = aptno;
+            this.StreetName = streetname;
+            this.City = city;
+            this.State = state;
+        }
+    }
+}
---
+++ Demos/10975/Mod6Demo2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod6Demo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Address homeaddress = new Address();
+            homeaddress.AptNo = 25;
+            homeaddress.StreetName = "Elwood";
+            homeaddress.City = "Northport";
+            homeaddress.State = "NY";
+            Console.WriteLine($"Address is {homeaddress.AptNo} {homeaddress.StreetName}, {homeaddress.City}, {homeaddress.State}");
+        }
+    }
+}
