09ee844851c9debc101433795c272fd95ad84d3d
Kyle Wood <97246548+huxley15@users.noreply.github.com>
2022-04-02 14:56:18 -0700

    Add files via upload

---
+++ Demos/10975/Mod5Demo1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod5Demo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //exception handling
+            int num1, num2;
+            Console.WriteLine("Enter 2 numbers:");
+            num1 = Int32.Parse(Console.ReadLine());
+            num2 = Int32.Parse(Console.ReadLine());
+
+            try
+            {
+                int result = num1 / num2;
+            }
+            catch(DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch(Exception ex)
+            {
+
+            }
+            finally
+            {
+                //mandatory code
+            }
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
