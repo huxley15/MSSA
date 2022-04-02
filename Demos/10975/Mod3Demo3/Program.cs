37e52886a301a21a1c5381dd1ffe6ed89f1d7a60
Kyle Wood <97246548+huxley15@users.noreply.github.com>
2022-04-02 14:52:03 -0700

    Add files via upload

---
+++ Demos/10975/Mod3Demo3/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod3Demo3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string favColor;
+            Console.WriteLine("Enter your favorite color:");
+            favColor = Console.ReadLine();
+
+            // Console.CapsLock 
+            // favColor.ToUpper
+            // favColor.ToLower
+            switch(favColor.ToLower())
+            {
+                case "Red":
+                case "red":
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Red it is!");
+                    break;
+                case "Green":
+                case "green":
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Green it is!");
+                    break;
+                case "Purple":
+                case "purple":
+                    Console.ForegroundColor = ConsoleColor.DarkMagenta;
+                    Console.WriteLine("Purple it is!");
+                    break;
+
+                default:
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine("Default color");
+                    break;
+            }
+
+            Console.ReadLine();
+
+            Console.WriteLine("Enter 5 numbers:");
+            int num;
+            int sum = 0;
+            for (int i = 0; i < 5; i++) //initialization, condition check, increment or decrement
+            {
+                num = Int32.Parse(Console.ReadLine());
+                sum = sum + num;
+            }
+            Console.WriteLine($"result is {sum}");
+            Console.ReadLine();
+        }
+    }
+}
